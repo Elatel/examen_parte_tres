80097abd951b42a5b736720e99553b87a99e679d
Felipe <felipeletelierc@gmail.com>
2020-09-21 20:35:31 -0300

    we made some users and now we have to add the name and picture to the data base

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_21_224954) do
+ActiveRecord::Schema.define(version: 2020_09_21_230431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,8 +32,13 @@ ActiveRecord::Schema.define(version: 2020_09_21_224954) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "image_url"
+    t.bigint "user_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["user_id"], name: "index_users_on_user_id"
   end
 
+  add_foreign_key "users", "users"
 end
