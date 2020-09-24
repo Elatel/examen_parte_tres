3548630d4ce8fb04a3271fb4c5bed58c7900df6c
Felipe <felipeletelierc@gmail.com>
2020-09-23 21:11:32 -0300

    we instal the kaminari gem and add the button for the next 50 tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,7 +4,7 @@ class TweetsController < ApplicationController
   # GET /tweets
   # GET /tweets.json
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.order(:content).page params[:page]
   end
 
   # GET /tweets/1
